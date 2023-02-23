3e90640878feeab584c7b75eb03c281bfe3f0306
Chad Wright <justdeejays@gmail.com>
2023-02-23 13:54:12 +0200

    feat: add desktop navbar & remove css animations

--- components/header.tsx
+++ components/header.tsx
@@ -44,111 +44,116 @@ const Header = ({ cssClasses }: Props) => {
 
   return (
     <header
-      className={`fixed w-full drop-shadow-md border-b-2 border-darkBlue ${cssClasses}`}
+      className={`fixed w-full drop-shadow-md border-b-2 border-darkBlue bg-beige ${cssClasses}`}
     >
-      {!menuToggle && (
-        <div
-          className={classNames(
-            "px-[20px] py-6 flex justify-between gap-3 phoneSmall:gap-6 bg-beige",
-            {
-              "h-[90px]": scrollPosition > 0,
-            }
-          )}
-        >
-          {/* logo */}
-          <animated.div style={logoLoadAnimate}>
-            <Link href="/" className="flex gap-4 items-center">
-              <Image
-                src={logo}
-                alt="Simone Snoyman Physiontherapy logo"
-                className={classNames(
-                  "hidden phoneTiny:block w-[50px] phoneSmall:w-[100px] h-auto transform ease-in-out duration-[150ms] ",
-                  {
-                    "scale-[45%] -translate-y-[43px] -translate-x-5":
-                      scrollPosition > 0,
-                  }
-                )}
-              />
-              <div>
-                <h1 className="flex flex-col">
-                  <span
-                    className={classNames(
-                      "font-amatic_sc text-[1.5rem] tracking-[0.0825rem] phoneSmall:tracking-[0.09625rem] phoneSmall:text-[1.75rem] text-lightBlue1 transform ease-in-out duration-[70ms]",
-                      {
-                        "opacity-0": scrollPosition > 0,
-                      }
-                    )}
-                  >
-                    SIMONE SNOYMAN
-                  </span>
-                  <span
-                    className={classNames(
-                      "font-bebas_neue text-[1.5rem] tracking-[0.0675rem] phoneSmall:text-[1.75rem] phoneSmall:tracking-[0.09rem] text-darkBlue -mt-2.5 -mb-2 transform ease-in-out duration-[70ms]",
-                      {
-                        "opacity-0": scrollPosition > 0,
-                      }
-                    )}
-                  >
-                    PHYSIOTHERAPY
-                  </span>
-                </h1>
-                <h2
-                  className={classNames(
-                    "font-lato font-thin text-[0.875rem] phoneSmall:text-[1rem] transform ease-in-out duration-[70ms]",
-                    {
-                      "opacity-0": scrollPosition > 0,
-                    }
-                  )}
-                >
-                  Plettenberg Bay
-                </h2>
-              </div>
-            </Link>
-          </animated.div>
-          <button
-            onClick={() => {
-              setMenuToggle(!menuToggle);
-              setMobileMenuToggle({ x: 0, opacity: 100 });
-            }}
+      <div className="tablet:pr-[50px] justify-between items-end tablet:flex tablet:py-6 desktop:max-w-[1400px] desktop:mx-auto">
+        {!menuToggle && (
+          <div
             className={classNames(
-              "h-16 w-16 grid place-items-center my-auto transform ease-in-out duration-[150ms]",
+              "px-[20px] tablet:pl-[50px] py-6 tablet:py-0 flex justify-between gap-3 phoneSmall:gap-6",
               {
-                "-translate-y-[10.5px]": scrollPosition > 0,
+                "h-[90px] tablet:h-[40px]": scrollPosition > 0,
               }
             )}
           >
-            <Image src={menuIcon} alt="Menu icon" className="w-[50px] h-auto" />
-          </button>
-        </div>
-      )}
+            {/* logo */}
+            <animated.div style={logoLoadAnimate}>
+              <Link href="/" className="flex gap-4 items-center">
+                <Image
+                  src={logo}
+                  alt="Simone Snoyman Physiontherapy logo"
+                  className={classNames(
+                    "hidden phoneTiny:block w-[50px] phoneSmall:w-[100px] h-auto tablet:w-[60px]",
+                    {
+                      "max-w-[45px] -translate-y-[17px] phoneSmall:-translate-y-[25px] tablet:max-w-[45px] tablet:translate-x-0 tablet:-translate-y-3.5":
+                        scrollPosition > 0,
+                    }
+                  )}
+                />
+                <div
+                  className={classNames("transform", {
+                    "opacity-0": scrollPosition > 0,
+                  })}
+                >
+                  <h1 className="flex flex-col">
+                    <span className="font-amatic_sc text-[1.5rem] tracking-[0.0825rem] phoneSmall:tracking-[0.09625rem] phoneSmall:text-[1.75rem] text-lightBlue1">
+                      SIMONE SNOYMAN
+                    </span>
+                    <span className="font-bebas_neue text-[1.5rem] tracking-[0.0675rem] phoneSmall:text-[1.75rem] phoneSmall:tracking-[0.09rem] text-darkBlue -mt-2.5 -mb-2">
+                      PHYSIOTHERAPY
+                    </span>
+                  </h1>
+                  <h2 className="font-lato font-thin text-[0.875rem] phoneSmall:text-[1rem] tablet:hidden">
+                    Plettenberg Bay
+                  </h2>
+                </div>
+              </Link>
+            </animated.div>
+            <button
+              onClick={() => {
+                setMenuToggle(!menuToggle);
+                setMobileMenuToggle({ x: 0, opacity: 100 });
+              }}
+              className={classNames(
+                "h-16 w-16 grid place-items-center my-auto transform tablet:hidden",
+                {
+                  "-translate-y-[10.5px]": scrollPosition > 0,
+                }
+              )}
+            >
+              <Image
+                src={menuIcon}
+                alt="Menu icon"
+                className="w-[50px] h-auto"
+              />
+            </button>
+          </div>
+        )}
 
-      {/* mobile navigation */}
-      {menuToggle && (
-        <nav className="bg-blue flex justify-between pt-12 pb-8 pl-[30px] pr-[20px]">
-          <animated.ul
-            style={mobileMenuToggle}
-            className="flex flex-col gap-6 font-lato font-extralight text-[1.5rem] text-white"
-          >
+        {/* mobile navigation */}
+        {menuToggle && (
+          <nav className="bg-blue flex justify-between pt-12 pb-8 pl-[30px] pr-[20px] tablet:hidden">
+            <animated.ul
+              style={mobileMenuToggle}
+              className="flex flex-col gap-6 font-lato font-extralight text-[1.5rem] text-white"
+            >
+              {menuList.map(({ title, url }, index) => (
+                <li key={index}>
+                  <Link href={url} className="py-2.5 px-3 -ml-3 transform">
+                    {title}
+                  </Link>
+                </li>
+              ))}
+            </animated.ul>
+            <button
+              onClick={() => setMenuToggle(!menuToggle)}
+              className="w-16 h-16 grid place-items-center -mt-2.5"
+            >
+              <Image
+                className="w-[50px] h-auto"
+                src={closeIcon}
+                alt="Close icon"
+              />
+            </button>
+          </nav>
+        )}
+
+        {/* desktop navigation */}
+
+        <nav
+          className={classNames("hidden tablet:block pb-1", {
+            "translate-y-[14px]": scrollPosition > 0,
+          })}
+        >
+          <ul className="flex gap-5 text-paragraph font-extralight">
             {menuList.map(({ title, url }, index) => (
-              <li key={index}>
-                <Link href={url} className="py-2.5 px-3 -ml-3 transform">
-                  {title}
-                </Link>
+              <li key={index} className="hover:text-blue">
+                <Link href={url}>{title}</Link>
               </li>
             ))}
-          </animated.ul>
-          <button
-            onClick={() => setMenuToggle(!menuToggle)}
-            className="w-16 h-16 grid place-items-center -mt-2.5"
-          >
-            <Image
-              className="w-[50px] h-auto"
-              src={closeIcon}
-              alt="Close icon"
-            />
-          </button>
+          </ul>
         </nav>
-      )}
+      </div>
     </header>
   );
 };
