862f31e11024795e8ef85d7d20b78f565e748b77
Chad Wright <justdeejays@gmail.com>
2023-02-27 16:21:57 +0200

    feat: add about-1 component

---
+++ components/about-1.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+import logo1 from "public/logos/hpcsa-logo.png";
+import logo2 from "public/logos/sasp-logo.jpg";
+
+const About1 = () => {
+  return (
+    <article>
+      <div className="grid grid-cols-1 items-center tablet:grid-cols-[75%_25%] gap-x-6">
+        <div className="flex flex-col tablet:flex-col gap-10 items-center tablet:order-2 tablet:row-span-2 tablet:place-self-start desktop:gap-5">
+          <p className="text-left">Proudly registered with:</p>
+          <div className="flex flex-wrap items-center gap-10 justify-center desktop:gap-5">
+            <Image
+              src={logo1}
+              alt="HPCSA logo"
+              className="max-w-[175px] h-auto bg-white p-3"
+            />
+            <Image
+              src={logo2}
+              alt="SASP logo"
+              className="max-w-[175px] h-auto bg-white p-3"
+            />
+          </div>
+        </div>
+        <p className="font-normal text-white bg-blue py-6 text-center my-16 tablet:order-3 tablet:my-0">
+          PR No. 072 000 0272329
+        </p>
+        <div className="tablet:order-1 place-self-start">
+          <p className="mb-4">
+            At Simone Snoyman Physiotherapy we believe in a “hands-on” approach.
+          </p>
+          <p>
+            Each patient is individually assessed and treated holistically with
+            emphasis placed on understanding the cause of your complaint and
+            empowering each person to achieve maximum function and pain relief.
+            I am passionate about what I do and believe that the therapist (me)
+            and patient (you) are a team with the goal being, “Getting YOU where
+            YOU want to be.”
+          </p>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default About1;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import About1 from "@/components/about-1";
 import Button, { ButtonVariant } from "@/components/button";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
@@ -9,120 +10,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Layout>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo
-          perspiciatis asperiores temporibus cupiditate error praesentium
-          recusandae deleniti. Accusamus fugit, eligendi saepe non voluptate
-          debitis dolor maiores fugiat iste inventore soluta quos eaque, sed
-          animi dicta dolore quo error voluptas recusandae laboriosam. Ex cum
-          excepturi quisquam, dolorem laboriosam cumque id harum perferendis
-          asperiores, placeat beatae ducimus! Amet odio nobis atque soluta quia
-          tempora molestiae libero debitis! Quas amet temporibus totam
-          distinctio beatae nesciunt, ipsum iure fugit nemo sapiente ut pariatur
-          eveniet optio molestias quod, magni ea dicta placeat est illum ratione
-          praesentium facere culpa nisi. Reprehenderit assumenda eligendi labore
-          autem a pariatur repellat expedita fuga saepe dolor excepturi, quia
-          atque sit recusandae debitis optio? Labore doloremque et beatae ea
-          sapiente pariatur minus atque corporis deserunt dolores hic, nostrum
-          cum voluptatem, sunt aspernatur mollitia sequi numquam cumque
-          excepturi laboriosam quam. Repellendus officia nam eligendi inventore
-          tempora voluptas culpa molestias illum doloremque quasi non fugit odit
-          at error asperiores delectus, quod magni voluptatibus voluptates
-          voluptatum? Illo fuga itaque magni deserunt sint unde vero quasi
-          numquam perferendis tempora velit saepe sapiente repudiandae iste
-          ipsum deleniti, fugit labore! Numquam minima dolore quaerat aperiam.
-          Atque, sapiente totam. Quo, sed odio sit similique accusamus nobis
-          beatae officiis vel praesentium incidunt, enim quidem nulla corrupti,
-          culpa explicabo voluptatem dolorem tempore ipsum? Harum eaque debitis
-          dicta dolorum ipsa neque officiis sint voluptatem obcaecati molestiae
-          corrupti corporis inventore explicabo veniam, quibusdam delectus!
-          Praesentium iusto commodi eos. Dolores laboriosam suscipit tempora a
-          distinctio eum, nesciunt quaerat unde perferendis error aut aspernatur
-          doloremque illum quibusdam voluptatibus esse in aliquam nulla cum.
-          Quia reprehenderit eum saepe maiores modi illum sint dolor sunt, ipsa
-          recusandae facilis, quas eius quibusdam, impedit aperiam commodi
-          accusamus. Voluptate, nobis illo aliquam, modi placeat nihil
-          perspiciatis consequatur porro dolores alias quasi! Distinctio ab at,
-          culpa est debitis, maiores molestias quidem, eum repellendus similique
-          deleniti voluptatem quibusdam expedita. Eos ipsam tenetur soluta?
-          Officiis, nisi mollitia a at, possimus repellendus expedita laudantium
-          est impedit repudiandae cum iusto voluptatibus maxime natus eligendi?
-          Dolorem architecto quod quo quae? Cum, odit fugit! Eos, fugiat
-          blanditiis dolorum voluptatem debitis cupiditate libero, magnam
-          obcaecati quam cumque inventore quo consequatur. Sit impedit
-          cupiditate saepe nihil consectetur minima sint numquam repellat vel
-          voluptatum vitae esse amet, dolore animi quis adipisci eius omnis
-          voluptates quisquam odio officia earum suscipit. Facilis placeat nemo
-          cupiditate tempora iure minus totam temporibus quas, labore velit,
-          veniam aut eaque corporis illo eligendi beatae quisquam excepturi ea!
-          Ad, deserunt saepe expedita quae libero alias veniam iste itaque
-          repellat praesentium necessitatibus sapiente magni quidem culpa earum
-          facere quisquam numquam, dolore nam amet atque accusantium? Assumenda,
-          repellendus ut? Nihil quod obcaecati earum, qui nisi autem commodi
-          sint veniam deserunt ab asperiores nobis inventore praesentium
-          exercitationem voluptatum modi rem enim! Quae tempore itaque, rerum
-          exercitationem harum provident et magnam iste fuga incidunt ullam quos
-          minima cupiditate iusto odio illo doloremque expedita, culpa accusamus
-          voluptas! Ratione eius aspernatur id est impedit, architecto soluta
-          nisi officia ab alias omnis ex nemo suscipit officiis minima libero
-          necessitatibus, facilis hic sed? Aliquam, maiores fugiat. Aut qui
-          dolor odit hic magnam et nemo voluptatibus beatae repellat nesciunt
-          unde architecto tempora, exercitationem, ad alias! Debitis cupiditate
-          fuga fugiat quos pariatur necessitatibus repudiandae sint tenetur
-          porro iure nihil ad laboriosam libero officia molestias a,
-          voluptatibus autem accusamus accusantium sit consequatur quaerat?
-          Asperiores sunt voluptatem incidunt ipsam optio eligendi, magnam unde
-          omnis, tempore consequuntur illum quisquam magni assumenda doloremque
-          atque tempora in ex fugit eveniet, sapiente quibusdam! Dicta autem
-          aliquid libero possimus, laborum in totam, consectetur at optio
-          doloribus ipsa sit, ipsam accusamus blanditiis nulla minus ullam
-          tempora dolore sint reiciendis. Illum accusamus, commodi aliquam,
-          consectetur deserunt, quis perferendis iste nobis et incidunt
-          consequatur! Illum unde nulla deleniti culpa eius sunt! Debitis soluta
-          sapiente nisi, nemo pariatur minus blanditiis nulla libero et ad iste
-          at voluptatem similique quo temporibus! Possimus labore porro
-          blanditiis inventore iure quam delectus, hic molestias. Provident
-          officia, consequuntur suscipit itaque vitae rem doloremque quos sunt
-          pariatur numquam a odit natus laudantium beatae voluptatem voluptates
-          est repellendus debitis non tempora doloribus. Voluptas hic vero ullam
-          voluptates quam similique nisi dolores debitis laborum, dolorem,
-          adipisci, dolorum corrupti illo minima illum nemo eos laboriosam.
-          Libero ratione eius placeat, suscipit ab facere impedit mollitia
-          voluptatem. Dicta quasi necessitatibus accusantium quisquam et vero
-          iusto vel, alias consequatur earum officiis maxime magnam dolore
-          consequuntur eum quam repellendus nemo ea unde sint enim! Vitae
-          voluptatum similique eos. In dolores recusandae veniam nulla voluptas
-          corrupti. Accusantium omnis quae rem fuga placeat excepturi eligendi,
-          iusto nulla explicabo molestias! Iusto tenetur nulla recusandae sit
-          veniam sequi unde repudiandae hic laborum sed illo ipsum, reiciendis
-          eos molestiae. Distinctio ipsa deleniti omnis voluptate officia sequi
-          at voluptatibus mollitia nam quibusdam dignissimos vero voluptatem
-          molestias exercitationem, dolor iusto obcaecati. Aliquam, dolores
-          dolorem! Sunt eum ipsam, eius perspiciatis officia cumque laborum.
-          Aliquam soluta fugiat totam ad maiores dolorem consequatur, id itaque
-          pariatur perspiciatis cum in eveniet porro nemo eum quos numquam rerum
-          deleniti provident. Rerum veritatis exercitationem repudiandae harum
-          pariatur ab perspiciatis amet sequi nisi, officia et maxime optio!
-          Eius possimus quisquam a facere voluptates illum provident molestias,
-          rerum magni in, cupiditate quaerat mollitia. Magni, quas cum. Vero,
-          tenetur accusamus doloribus optio quaerat qui cupiditate ipsam quia
-          excepturi nemo tempora dignissimos eveniet iure, nam eos? Voluptatem
-          non excepturi ipsa laudantium vitae similique! Dolores eum officia ea
-          voluptatem? Itaque quos a, iste voluptates repellendus perferendis
-          laborum obcaecati fuga numquam cupiditate enim culpa quidem nulla
-          omnis accusamus quo provident molestiae. Magni porro tenetur id,
-          accusantium alias atque sunt nulla voluptatem nihil modi quo deleniti
-          vel. Saepe, soluta similique? Nulla dolorem dicta, harum impedit cum
-          esse magni alias debitis soluta placeat deserunt cupiditate nesciunt
-          odit dolorum excepturi corporis et laborum aliquid omnis ex. Nihil
-          esse, reprehenderit quae ex, magnam exercitationem natus fugiat
-          consectetur praesentium commodi alias animi ut ducimus tempore error
-          recusandae a amet incidunt cum voluptatibus perspiciatis totam. Porro,
-          consectetur cum veritatis incidunt est harum reprehenderit ipsum
-          pariatur tempora ad optio? Exercitationem maiores aspernatur animi
-          officia cum excepturi doloribus, nemo officiis, quia pariatur
-          voluptates enim atque dolor molestiae.
-        </p>
+        <About1 />
       </Layout>
     </div>
   );
