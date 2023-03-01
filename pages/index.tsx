68accc4b08b2e85acd20c9dcace407f19297802f
Chad Wright <justdeejays@gmail.com>
2023-03-01 14:33:36 +0200

    feat: add who am i component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import About1 from "@/components/about-1";
+import About2 from "@/components/about-2";
 import Conditions from "@/components/conditions";
 import Fees from "@/components/fees";
 import Header from "@/components/header";
@@ -12,11 +13,12 @@ export default function Home() {
       <Header />
       <Hero />
       <Layout>
-        <About1 cssClasses="mt-6" />
+        <About1 cssClasses="mt-12" />
         <div className="desktop:flex flex-wrap">
-          <Conditions cssClasses="my-20" />
+          <Conditions cssClasses="my-32" />
           <Fees cssClasses="desktop:hidden" />
-          <Treatments cssClasses="my-20" />
+          <Treatments cssClasses="my-32" />
+          <About2 />
         </div>
       </Layout>
     </div>
