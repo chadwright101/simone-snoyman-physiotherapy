4cda3e83a7f53917e10d30b55c8554346868e10a
Chad Wright <justdeejays@gmail.com>
2023-03-02 13:23:39 +0200

    feat: add testimonials component

---
+++ components/testimonials.tsx
@@ -0,0 +1,51 @@
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+
+import "@splidejs/react-splide/css";
+
+import testimonialsList from "../data/testimonials-test-data.json";
+
+interface Props {
+  cssClasses?: string;
+}
+
+const Testimonials = ({ cssClasses }: Props) => {
+  return (
+    <article className={`${cssClasses}`}>
+      <h2 className="text-heading uppercase font-bebas_neue text-center tablet:text-left phoneLarge:place-self-start mb-10">
+        Testimonials
+      </h2>
+      <Splide
+        options={{
+          arrows: false,
+          height: "12rem",
+          direction: "ttb",
+          autoplay: true,
+          type: "loop",
+          wheel: true,
+          rewind: true,
+          breakpoints: {
+            1300: {
+              height: "14rem",
+            },
+          },
+        }}
+        className="border-y-2 border-black"
+      >
+        {testimonialsList.map(({ quote, name, location }, index) => (
+          <SplideSlide
+            key={index}
+            className="flex justify-center items-center tablet:justify-start"
+          >
+            <div className="pr-10">
+              <blockquote className="italic mb-4">{quote}</blockquote>
+              <p className="font-normal">{name}</p>
+              <p className="font-thin">{location}</p>
+            </div>
+          </SplideSlide>
+        ))}
+      </Splide>
+    </article>
+  );
+};
+
+export default Testimonials;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Fees from "@/components/fees";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Layout from "@/components/layout";
+import Testimonials from "@/components/testimonials";
 import Treatments from "@/components/treatments";
 
 export default function Home() {
@@ -13,12 +14,16 @@ export default function Home() {
       <Header />
       <Hero />
       <Layout>
-        <About1 cssClasses="mt-12" />
+        <About1 cssClasses="mt-10" />
         <div className="desktop:flex flex-wrap">
-          <Conditions cssClasses="my-32" />
+          <Conditions cssClasses="my-24" />
           <Fees cssClasses="desktop:hidden" />
-          <Treatments cssClasses="my-32" />
+          <Treatments cssClasses="my-24" />
           <About2 />
+        </div>
+        <div className="tablet:grid grid-cols-1 desktop:grid-cols-2 gap-8 my-24">
+          <Fees cssClasses="hidden desktop:flex flex-col" />
+          <Testimonials />
         </div>
       </Layout>
     </div>
