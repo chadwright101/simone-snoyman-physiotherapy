d34005a68b902626355e635944278d852d2f6583
Chad Wright <justdeejays@gmail.com>
2023-03-05 12:41:10 +0200

    feat: add id tags for navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,18 +16,34 @@ export default function Home() {
       <Header />
       <Hero />
       <Layout>
+        <div id="about" className="-translate-y-36"></div>
         <About1 cssClasses="mt-10" />
         <div className="desktop:flex flex-wrap">
+          <div
+            id="treatments"
+            className="-translate-y-32 desktop:-translate-y-20"
+          ></div>
           <Conditions cssClasses="my-24" />
-          <Fees cssClasses="desktop:hidden" />
+          <div>
+            <div id="fees" className="desktop:hidden -translate-y-32"></div>
+            <Fees cssClasses="desktop:hidden" />
+          </div>
           <Treatments cssClasses="my-24" />
-          <About2 />
+          <div>
+            <div id="your-physio" className="-translate-y-32"></div>
+            <About2 />
+          </div>
         </div>
+        <div
+          id="fees"
+          className="hidden desktop:block desktop:-translate-y-14"
+        ></div>
         <div className="tablet:grid grid-cols-1 desktop:grid-cols-2 gap-8 my-24">
           <Fees cssClasses="hidden desktop:flex flex-col" />
           <Testimonials />
         </div>
       </Layout>
+      <div id="contact" className="-translate-y-32"></div>
       <Contact cssClasses="mb-20" />
       <Layout>
         <Footer cssClasses="pb-8 tablet:pb-6 desktop:pb-4" />
