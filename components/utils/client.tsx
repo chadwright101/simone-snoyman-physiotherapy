432d618012eeca012110939cb10deae5bcd43e94
Chad Wright <justdeejays@gmail.com>
2023-03-10 20:00:54 +0200

    feat: add xneelo wordpress graphql url

--- components/utils/client.tsx
+++ components/utils/client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://simone-snoyman-physiotherapy.local/graphql",
+  uri: "http://simonesnoymanphysiotherapy.co.za/graphql",
   cache: new InMemoryCache(),
 });
 
