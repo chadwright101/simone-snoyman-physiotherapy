6b0aaaad3c11ec115ad8a9c27dedbea7663a8012
Chad Wright <justdeejays@gmail.com>
2023-03-11 16:26:24 +0200

    fix: add new apollo client url

--- components/utils/client.tsx
+++ components/utils/client.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://simonesnoymanphysiotherapy.co.za/graphql",
+  uri: "http://wordpress.simonesnoymanphysiotherapy.co.za/graphql",
   cache: new InMemoryCache(),
 });
 
